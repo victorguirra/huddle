b3caa2bfcdda10ab8be1011734678000f3e0267e
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-05 21:20:25 -0300

    create hero section

---
+++ src/pages/Home/Hero/index.tsx
@@ -0,0 +1,21 @@
+import { Container, Title, Description } from './styles';
+
+import Button from '../../../components/Button';
+
+export default function Hero() {
+    return(
+        <Container>
+            <Title>
+                Build The Community Your Fans Will Love
+            </Title>
+
+            <Description>
+                Huddle re-imagines the way build communities. You have a <br />
+                voice, but so does your audience. Create connections with your <br />
+                users as you engage in genuine discussion.
+            </Description>
+
+            <Button>Get Started For Free</Button>
+        </Container>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Container } from './styles'
 
 import Header from '../../components/Header';
+import Hero from './Hero';
 
 export default function Home() {
     return(
         <Container>
             <Header />
+            <Hero />
         </Container>
     )
 }
