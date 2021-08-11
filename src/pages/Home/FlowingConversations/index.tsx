8a830acb7731fbc4ab4f6d218d50e292c777ad24
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-10 21:29:24 -0300

    create flowing converastions section

--- src/pages/Home/FlowingConversations/index.tsx
+++ src/pages/Home/FlowingConversations/index.tsx
@@ -1,9 +1,25 @@
-import { Container } from './styles';
+import { Wrapper, Container, Content, Image, BackgroundImage } from './styles';
+
+import illustraion from '../../../assets/images/illustrations/flowing-conversation.svg';
+import bgImg from '../../../assets/images/background/desktop/section-top-desktop-2.svg';
 
 export default function FlowingConversations() {
     return(
-        <Container>
-            <h1>Hello World</h1>
-        </Container>
+        <Wrapper>
+            <Container>
+                <Image src={ illustraion } alt="Flowing Conversations" />
+
+                <Content>
+                    <h1>Flowing Conversations</h1>
+
+                    <p>
+                        You wouldn't paginate a conversation in real life, so why do it online? <br/>
+                        Our threads have just-in-time loading for a more natural flow.
+                    </p>
+                </Content>
+            </Container>
+
+            <BackgroundImage src={ bgImg } alt="Wave" />
+        </Wrapper>
     )
 }
--- src/pages/Home/FlowingConversations/styles.ts
+++ src/pages/Home/FlowingConversations/styles.ts
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Wrapper = styled.div`
+    margin-top:20rem;
+    position:relative;
+`;
+
+export const Container = styled.div`
+    padding:0 10rem;
+
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+`;
+
+export const Content = styled.div`
+    p {
+        margin-top:2rem;
+    }
+`;
+
+export const Image = styled.img`
+    width:30%;
+    max-width:48%;
+`;
+
+export const BackgroundImage = styled.img`
+    width:100%;
+    position:absolute;
+
+    margin-top:10rem;
+`;
