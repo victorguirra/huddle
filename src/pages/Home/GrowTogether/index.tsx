862cfa920af10878ff66e7b8e683cdd498c77bce
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-10 21:10:21 -0300

    feat: create grow together section

--- src/pages/Home/GrowTogether/index.tsx
+++ src/pages/Home/GrowTogether/index.tsx
@@ -1,9 +1,21 @@
-import { Container } from './styles';
+import { Wrapper, Container, Image } from './styles';
+
+import illustration from '../../../assets/images/illustrations/grow-together.svg';
 
 export default function GrowTogether() {
     return(
-        <Container>
-            <h1>Hello World</h1>
-        </Container>
+        <Wrapper>
+            <Container>
+                <h1>Grow Together</h1>
+
+                <p>
+                    Generate meaningful discussions with your audience and build a <br/>
+                    strong, loyal community. Think of the insighful conversations you <br/>
+                    miss out on with a feedback form.
+                </p>
+            </Container>
+
+            <Image src={ illustration } alt="Grow Together" />
+        </Wrapper>
     )
 }
--- src/pages/Home/GrowTogether/styles.ts
+++ src/pages/Home/GrowTogether/styles.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
     background-color:${({ theme }) => theme.colors.paleBlue };
+
+    padding:0 10rem;
+
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+`;
+
+export const Container = styled.div`
+    p {
+        margin-top:2rem;
+    }
+`;
+
+export const Image = styled.img`
+    width:30%;
+    max-width:48%;
 `;
