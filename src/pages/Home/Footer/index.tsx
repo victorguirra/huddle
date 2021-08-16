31fa530675aae27e585236e211622bed40116560
victorguirra <victor.hugo.guirra@gmail.com>
2021-08-16 20:41:10 -0300

    create footer

--- src/pages/Home/Footer/index.tsx
+++ src/pages/Home/Footer/index.tsx
@@ -1,6 +1,18 @@
-import { Wrapper, BackgroundImage, Container } from './styles';
+import {
+    Wrapper, 
+    BackgroundImage, 
+    Container, 
+    InfoContainer, 
+    FormContainer, 
+    Form
+} from './styles';
+
+import Button from '../../../components/Button';
 
 import bgImg from '../../../assets/images/background/desktop/footer-top-desktop.svg';
+import logoImg from '../../../assets/images/logo_secondary.svg';
+import phoneIcon from '../../../assets/images/icons/phone.svg';
+import emailIcon from '../../../assets/images/icons/email.svg';
 
 export default function Footer() {
     return(
@@ -8,7 +20,40 @@ export default function Footer() {
             <BackgroundImage src={ bgImg } alt="Wave" />
             
             <Container>
-                <h1>Footer</h1>
+                <InfoContainer>
+                    <img src={ logoImg } alt="Logo" />
+                    
+                    <p>
+                        Lorem ipsum dolor sit amet, consectur <br/>
+                        adipscing elt. Maurus mulla quam, handertir <br/>
+                        lacina vestibulul a, ultirces quis sem.
+                    </p>
+
+                    <div className="contact-container">
+                        <img src={ phoneIcon } alt="Phone" />
+                        <span>Phone: +1-543-123-4567</span>
+                    </div>
+
+                    <div className="contact-container">
+                        <img src={ emailIcon } alt="Emails" />
+                        <span>example@huddle.com</span>
+                    </div>
+                </InfoContainer>
+
+                <FormContainer>
+                    <h2>NewsLetter</h2>
+
+                    <p>
+                        To recieve on how to grow your community, <br/>
+                        sign up to our weekly newsletter. We'll never send <br/>
+                        you span or pass on your email address.
+                    </p>
+
+                    <Form>
+                        <input type="email" placeholder="Your email address" />
+                        <Button type="submit">Subscribe</Button>
+                    </Form>
+                </FormContainer>
             </Container>
         </Wrapper>
     )
--- src/pages/Home/Footer/styles.ts
+++ src/pages/Home/Footer/styles.ts
@@ -12,5 +12,72 @@ export const BackgroundImage = styled.img`
     top:-13rem;
 `;
 
-export const Container = styled.div`
+export const Container = styled.div`    
+    padding:7rem;
+
+    display:flex;
+    align-items:flex-start;
+    justify-content:space-between;
+`;
+
+export const InfoContainer = styled.div`
+    p {
+        color:${({ theme }) => theme.colors.white };
+        line-height:1.6rem;
+        margin-top:1.2rem;
+    }
+
+    .contact-container {
+        display:flex;
+        align-items:center;
+
+        margin-top:1.2rem;
+
+        span {
+            color:${({ theme }) => theme.colors.white };
+            margin-left:1.2rem;
+        }
+    }
+`;
+
+export const FormContainer = styled.form`
+    h2 {
+        color:${({ theme }) => theme.colors.white };
+        text-transform:uppercase;
+    }
+
+    p {
+        color:${({ theme }) => theme.colors.white };
+        line-height:1.6rem;
+        margin-top:1.2rem;
+    }
+`;
+
+export const Form = styled.form`
+    margin-top:2.4rem;
+    
+    input {
+        width:24rem;
+        height:3.8rem;
+
+        border:3px solid transparent;
+        border-radius:.4rem;
+
+        padding:0 .8rem;
+        outline:none;
+
+        transition:border-color .2s ease-in;
+
+        &:focus {
+            border:3px solid ${({ theme }) => theme.colors.pink };
+        }
+    }
+
+    button {
+        width:14rem;
+        height:3.8rem;
+
+        border-radius:.4rem;
+        margin-left:3rem;
+    }
 `;
